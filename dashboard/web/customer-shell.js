c846b8d0f522214b371b518c8b7315b1ef3a7524
Ezequiel Alécio Ribeiro <ezequiel.ribeiro@gmail.com>
2026-08-27 10:59:08 -0300

    Load customer navigation before dashboard core

--- dashboard/web/customer-shell.js
+++ dashboard/web/customer-shell.js
@@ -3,7 +3,7 @@
   function load(src) {
     const script = document.createElement("script");
     script.src = src;
-    script.defer = false;
+    script.async = false;
     document.head.appendChild(script);
   }
   load("/customer-navigation.js?v=1");
