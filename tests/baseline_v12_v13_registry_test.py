f7a86d5ba47a79a96b2660b49158903d4dada3d0
Ezequiel Alécio Ribeiro <ezequiel.ribeiro@gmail.com>
2026-09-22 22:17:59 -0300

    test(database): advance baseline registry contract through v21

--- tests/baseline_v12_v13_registry_test.py
+++ tests/baseline_v12_v13_registry_test.py
@@ -13,19 +13,21 @@ if str(DATABASE) not in sys.path:
 from baseline_upgrade_engine import UPGRADES, latest_upgrade_version
 
 
-class BaselineV12V20RegistryTest(unittest.TestCase):
-    def test_registry_is_contiguous_through_v20(self) -> None:
-        self.assertEqual([upgrade.version for upgrade in UPGRADES], list(range(1, 21)))
-        self.assertEqual(latest_upgrade_version(), 20)
-        self.assertEqual(UPGRADES[-9].name, "universal_content_update")
-        self.assertEqual(UPGRADES[-8].name, "maintenance_restart_framework")
-        self.assertEqual(UPGRADES[-7].name, "yarax_admin_operations")
-        self.assertEqual(UPGRADES[-6].name, "dayz_native_restart_commands")
-        self.assertEqual(UPGRADES[-5].name, "generic_native_restart_commands")
-        self.assertEqual(UPGRADES[-4].name, "database_intelligence")
-        self.assertEqual(UPGRADES[-3].name, "operation_diagnostics")
-        self.assertEqual(UPGRADES[-2].name, "alert_customer_identity")
-        self.assertEqual(UPGRADES[-1].name, "legacy_minecraft_contract_products")
+class BaselineV12V21RegistryTest(unittest.TestCase):
+    def test_registry_is_contiguous_through_v21(self) -> None:
+        self.assertEqual([upgrade.version for upgrade in UPGRADES], list(range(1, 22)))
+        self.assertEqual(latest_upgrade_version(), 21)
+        self.assertEqual(UPGRADES[-10].name, "universal_content_update")
+        self.assertEqual(UPGRADES[-9].name, "maintenance_restart_framework")
+        self.assertEqual(UPGRADES[-8].name, "yarax_admin_operations")
+        self.assertEqual(UPGRADES[-7].name, "dayz_native_restart_commands")
+        self.assertEqual(UPGRADES[-6].name, "generic_native_restart_commands")
+        self.assertEqual(UPGRADES[-5].name, "database_intelligence")
+        self.assertEqual(UPGRADES[-4].name, "operation_diagnostics")
+        self.assertEqual(UPGRADES[-3].name, "alert_customer_identity")
+        self.assertEqual(UPGRADES[-2].name, "legacy_minecraft_contract_products"
+        )
+        self.assertEqual(UPGRADES[-1].name, "dayz_management_operations")
 
 
 if __name__ == "__main__":
